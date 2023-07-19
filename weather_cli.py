35daf0b75e29d38e46915196c81df211524af04f
Rohith Namala <124754649+rohithgitz@users.noreply.github.com>
2023-07-19 10:11:38 +0530

    weather_cli.py

--- weather_cli.py
+++ weather_cli.py
@@ -2,31 +2,43 @@ import requests
 
 def get_weather_data(city):
     url = f"https://samples.openweathermap.org/data/2.5/forecast/hourly?q={city}&appid=b6907d289e10d714a6e88b30761fae22"
-    response = requests.get(url)
-    data = response.json()
-    return data
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise exception if the API call fails
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print("Error occurred during API call. Please check your internet connection.")
+        return None
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return None
 
 def get_temperature_by_date(data, date):
-    for entry in data['list']:
-        if date in entry['dt_txt']:
+    for entry in data.get('list', []):
+        if date in entry.get('dt_txt', ''):
             return entry['main']['temp']
     return None
 
 def get_wind_speed_by_date(data, date):
-    for entry in data['list']:
-        if date in entry['dt_txt']:
+    for entry in data.get('list', []):
+        if date in entry.get('dt_txt', ''):
             return entry['wind']['speed']
     return None
 
 def get_pressure_by_date(data, date):
-    for entry in data['list']:
-        if date in entry['dt_txt']:
+    for entry in data.get('list', []):
+        if date in entry.get('dt_txt', ''):
             return entry['main']['pressure']
     return None
 
 def main():
     city = input("Enter the city name: ")
     data = get_weather_data(city)
+
+    if not data:
+        print("Exiting the program.")
+        return
 
     while True:
         print("\nOptions:")
